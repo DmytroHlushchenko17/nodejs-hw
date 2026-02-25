c13211369af16db26eabfd222a5f87db177dc945
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-02-25 17:29:16 +0200

    add folder controllers and logik routers

--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -1,19 +1,11 @@
 import { Router } from 'express';
-import { Student } from '../models/student';
+import {
+  getStudents,
+  getStudentsById,
+} from '../controllers/studentsController.js';
 
 const router = Router();
-router.get('/students', async (req, res) => {
-  const students = await Student.find();
-  res.status(200).json(students);
-});
+router.get('/students', getStudents);
 
-router.get('/students/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-  const student = await Student.findById(studentId);
-
-  if (!student) {
-    return res.status(404).json({ message: 'Student not found' });
-  }
-  res.status(200).json(student);
-});
+router.get('/students/:studentId', getStudentsById);
 export default router;
