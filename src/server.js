4796b43609edef213c0ed9ccf83a50ef4154438f
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-02-28 17:48:23 +0200

    chandge database on notes and rename logik on notes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { connectMongoDB } from './db/connectMongo.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import dns from 'dns';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
-import studentsRoutes from './routes/studentsRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 dns.setServers(['1.1.1.1', '8.8.8.8']);
 
@@ -25,23 +25,7 @@ app.use((req, res, next) => {
   console.log(`Time:${new Date().toLocaleString()}`);
   next();
 });
-
-app.use('/students', studentsRoutes);
-
-// app.get('/notes', (req, res) => {
-//   console.log(req.body);
-//   res.status(200).json({ message: 'Retrieved all notes' });
-// });
-
-// app.get('/notes/:noteId', (req, res) => {
-//   const id_param = req.params.noteId;
-//   res.status(200).json({ message: `Retrieved note with ID: ${id_param}` });
-// });
-
-// app.get('/test-error', (req, res) => {
-//   throw new Error('Something went erong');
-// });
-
+app.use('/notes', notesRoutes);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
