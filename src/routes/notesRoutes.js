c9849a8fb857ad5217a580530a01bc3319e817cb
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-03-04 21:19:10 +0200

    add:foo validation on get,patch,delete

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -7,18 +7,25 @@ import {
   updateNote,
 } from '../controllers/notesController.js';
 import { celebrate } from 'celebrate';
-import { createNotesSchema } from '../validations/notesValidation.js';
+import {
+  createNotesSchema,
+  createTagSchema,
+  noteIdParamSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
 
 const notesRouter = Router();
 
 notesRouter.get('/', getAllNotes);
 
-notesRouter.get('/:noteId', getNoteById);
+notesRouter.get('/:noteId', celebrate(noteIdParamSchema), getNoteById);
+
+notesRouter.get('/:tag', celebrate(createTagSchema), getAllNotes);
 
 notesRouter.post('/', celebrate(createNotesSchema), createNote);
 
-notesRouter.delete('/:noteId', deleteNote);
+notesRouter.delete('/:noteId', celebrate(noteIdParamSchema), deleteNote);
 
-notesRouter.patch('/:noteId', updateNote);
+notesRouter.patch('/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default notesRouter;
